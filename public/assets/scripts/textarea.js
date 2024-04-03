5c881340c5b65132d669c3a7f2067cd3cc42c10c
Bruno Andrade <brunoan99@gmail.com>
2024-04-03 13:21:07 -0300

    feat: improve multiline textarea responsiveness

--- public/assets/scripts/textarea.js
+++ public/assets/scripts/textarea.js
@@ -1,15 +1,4 @@
 /*
-  Change element height to exand when Text area get new rows.
-*/
-function OnInput() {
-  this.style.height = 0;
-  this.style.height = (this.scrollHeight) + "px";
-}
-const tx = document.getElementById("text-area-buffer");
-tx.setAttribute("style", "height:" + (tx.scrollHeight) + "px;overflow-y:hidden;");
-tx.addEventListener("input", OnInput, false);
-
-/*
   Change scroll position to follow new output lines
 */
 const tm = document.getElementById("terminal");
